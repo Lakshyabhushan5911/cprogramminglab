2cca3a70cfe7632fac4b0c2d394c7d636296a719
Lakshyabhushan5911 <146870483+Lakshyabhushan5911@users.noreply.github.com>
2023-11-20 01:16:52 +0530

    Add files via upload

---
+++ arraymagic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int arr[10]'
+    int i,n;
+    printf("enter the size of an array");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+{
+    printf("\n ennter the %d value of an array);
+scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+printf("\n%d %d %d %d",arr[i],*arr[i],*(i+arr),*arr+1,i[arr]);
+}
+}
